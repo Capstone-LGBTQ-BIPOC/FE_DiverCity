09400c1360d396b25ff1603c0935470a2dc7ce04
Ronald Head <ronaldlhead@gmail.com>
2022-05-13 14:57:07 -0600

    Add Categories component to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,14 @@
+import * as React from 'react';
+import Categories from '../Categories/Categories';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
- 
+  return(
+    <main>
+      <Categories />
+    </main>
+  )
 }
 
 export default App;
