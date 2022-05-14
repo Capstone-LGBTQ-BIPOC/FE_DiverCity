86d0a76617ae322452a618bc2e7e18d33bdd310c
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-05-14 14:41:55 -0600

    Add categeory card to categories

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
+import CategoryCard from '../CategoryCard/CategoryCard';
 import './Categories.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fa-utensils, fa-bag-shopping } from '@fortawesome/fontawesome-free-solid'
 
 function Categories() {
-  return(
+  return (
     <section>
-      
+      <CategoryCard icon={<FontAwesomeIcon icon='fa-solid fa-utensils' />} />
+      <CategoryCard icon={<FontAwesomeIcon icon="fa-solid fa-bag-shopping" />} />
+      <CategoryCard />
     </section>
   )
 }
