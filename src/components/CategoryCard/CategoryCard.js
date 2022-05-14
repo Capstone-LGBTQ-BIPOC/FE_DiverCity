dba3051a63e3b77db84dbd513e1fdd689d44d0da
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-05-14 14:41:32 -0600

    Create category card

---
+++ src/components/CategoryCard/CategoryCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function CategoryCard({icon}) {
+  return (
+    <section>
+      <h1></h1>
+      <button>View All</button>
+      {icon}
+    </section>
+  )
+}
+
+export default CategoryCard
