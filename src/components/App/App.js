861e61126c26e01f76c30557b3d1275ab76df2f9
Kim Ward <kmewrd@outlook.com>
2022-05-16 16:43:28 -0500

    Add homepage route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,18 @@ import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LocationContextProvider } from '../../context/LocationContext/LocationContext';
 import BusinessContextProvider from '../../context/BusinessData/BusinessContext';
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
     <LocationContextProvider>
-    <BusinessContextProvider>
-      <main>
-        <Categories />
-      </main>
-    </BusinessContextProvider>
+      <BusinessContextProvider>
+        <main>
+          <Routes>
+            <Route path='/' element={<Categories />} />
+          </Routes>
+        </main>
+      </BusinessContextProvider>
     </LocationContextProvider>
   )
 }
