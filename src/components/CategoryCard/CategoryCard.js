69af8c552cb6c7eec6a1e01955de7150fe7ffd7a
Kim Ward <kmewrd@outlook.com>
2022-05-16 17:15:18 -0500

    Add context hook and invoke on button click

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useContext } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { BusinessContext } from '../../context/BusinessData/BusinessContext';
 
 function CategoryCard({ icon, type, label }) {
+  const biz = useContext(BusinessContext);
+
   return (
     <section>
       <h1>{label}</h1>
       <FontAwesomeIcon icon={icon} />
-      <button>View All</button>
+      <button onClick={() => biz.getBusinesses(type)}>View All</button>
     </section>
   )
 }
