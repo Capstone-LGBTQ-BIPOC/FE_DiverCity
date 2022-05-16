f7a09f99e6699897c6c4eb288cecf2e98d149b8e
Kim Ward <kmewrd@outlook.com>
2022-05-16 17:14:28 -0500

    Fix import

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,7 @@
-import * as React from 'react';
 import Categories from '../Categories/Categories';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { LocationContextProvider } from '../../context/LocationContext/LocationContext';
-import BusinessContextProvider from '../../context/BusinessData/BusinessContext';
+import { BusinessContextProvider } from '../../context/BusinessData/BusinessContext';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
