3f2e162e3cc7fea5614851216087d4551d13d802
Casey Halstead <catherineclosee@gmail.com>
2022-05-16 15:46:21 -0500

    Add context for business data

---
+++ src/context/BusinessData/BusinessContext.js
@@ -0,0 +1,10 @@
+import { createContext, useState } from "react";
+
+const BusinessContext = createContext([]);
+
+const BusinessContextProvider = () => {
+
+
+}
+
+export default BusinessContextProvider;
