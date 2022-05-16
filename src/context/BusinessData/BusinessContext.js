44337dc89f8d5aa4d6a64873e2cb7b13af1e3c25
Casey Halstead <catherineclosee@gmail.com>
2022-05-16 15:58:12 -0500

    Add location variable to use location context for fetch

--- src/context/BusinessData/BusinessContext.js
+++ src/context/BusinessData/BusinessContext.js
@@ -1,9 +1,13 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useContext } from "react";
+import fetchBusinesses from "../../apiCalls";
 
 const BusinessContext = createContext([]);
 
 const BusinessContextProvider = () => {
+  
+  const [businesses, setBusinesses] = useState([])
 
+  const location = useContext(LocationContext)
 
 }
 
