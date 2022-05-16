98873c0dfc30bbad8d0dd8ab6a268935e7a61a5e
Casey Halstead <catherineclosee@gmail.com>
2022-05-16 16:14:11 -0500

    Add Business Context Provider to App return

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,16 @@ import Categories from '../Categories/Categories';
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import LocationContextProvider from '../../context/LocationContext/LocationContext';
+import BusinessContextProvider from '../../context/BusinessData/BusinessContext';
 
 const App = () => {
   return (
     <LocationContextProvider>
+    <BusinessContextProvider>
       <main>
         <Categories />
       </main>
+    </BusinessContextProvider>
     </LocationContextProvider>
   )
 }
