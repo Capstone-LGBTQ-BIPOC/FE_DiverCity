af0e9f8af1a4640e195acb8b298b8beb0423f9a0
Ronald Head <ronaldlhead@gmail.com>
2022-05-16 18:46:02 -0600

    Set up Listings component

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,17 +1,22 @@
 import { useContext } from 'react';
 import { BusinessContext } from '../../context/BusinessData/BusinessContext';
 import BusinessCard from '../BusinessCard/BusinessCard';
-
 import './Listings.css';
 
 function Listings() {
   const biz = useContext(BusinessContext);
-  const businessListings = biz.map(biz => {
-   return <BusinessCard name={biz.attributes.name} image={biz.attributes.image} />
+
+  const businessListings = biz.businesses.map(business => {
+   return <BusinessCard name={business.attributes.name} image={business.attributes.image} key={business.id}/>
   });
-  
+
   return(
-    <section>{businessListings}</section>
+    <section>
+      <header>
+        <h2>{biz.category}</h2>
+      </header>
+      {businessListings}
+    </section>
   )
 }
 
