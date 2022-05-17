ac27754b86d3f0754cc09efab09ab179fcb029c6
Kim Ward <kmewrd@outlook.com>
2022-05-17 17:38:13 -0500

    Add filter state tied to dropdown menu

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -1,10 +1,12 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { BusinessContext } from '../../context/BusinessData/BusinessContext';
 import BusinessCard from '../BusinessCard/BusinessCard';
 import './Listings.css';
 
 function Listings() {
   const biz = useContext(BusinessContext);
+
+  const [filter, setFilter] = useState('');
 
   const businessListings = biz.businesses.map(business => {
    return <BusinessCard name={business.attributes.name} image={business.attributes.image} key={business.id}/>
@@ -13,6 +15,10 @@ function Listings() {
   return(
     <section>
       <h2>{biz.category}</h2>
+      <select value={filter} onChange={e => setFilter(e.target.value)}>
+        <option value=''>Show All</option>
+        {/* {options} */}
+      </select>
       {businessListings}
     </section>
   )
