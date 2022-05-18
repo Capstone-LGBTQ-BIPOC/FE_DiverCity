5711333064c07f8b7b3db1466f6852ab6b8f5883
Kim Ward <kmewrd@outlook.com>
2022-05-18 18:25:33 -0500

    Add error state and catch to fetch call

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,15 +3,17 @@ import { fetchBusiness } from '../../apiCalls';
 
 const Modal = ({ id }) => {
   const [business, setBusiness] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchBusiness(id)
       .then(data => setBusiness(data))
+      .catch(err => setError('Oops, something went wrong! Please try again later.'))
   }, [])
 
   return (
     <div>
-
+      {error && <p>{error}</p>}
     </div>
   )
 }
