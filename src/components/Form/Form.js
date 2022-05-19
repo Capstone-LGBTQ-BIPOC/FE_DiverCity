4a1fc096e7651bdf38e01a39af9bcbf4407ccfde
Emili Kaiman (she/her) <93098905+Ekaiman@users.noreply.github.com>
2022-05-18 19:14:52 -0600

    Add display for current search and use current location button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,40 +1,59 @@
 import React from 'react'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { LocationContext } from '../../context/LocationContext/LocationContext'
 
-const Form = (props) => {
+const Form = props => {
   const location = useContext(LocationContext)
-  
+
+  const [typed, setTyped] = useState('')
+
   const submitLocation = event => {
     event.preventDefault()
     clearInputs()
   }
 
   const clearInputs = () => {
-    location.setSelectedLocation('')
+    setTyped('')
   }
 
   return (
-    <form>
-      <input
-        type='text'
-        placeholder='Prompt'
-        name='selectedLocation'
-        value={location.selectedLocation}
-        onChange={event => {
-          location.setSelectedLocation(event.target.value)
-        }}
-        required
-      />
+    <div>
+      {!location.selectedLocation && (
+        <h2>Youre currently searching in {location.location.city}</h2>
+      )}
+      {location.selectedLocation && (
+        <h2>Youre currently searching in {location.selectedLocation}</h2>
+      )}
+      <form>
+        <input
+          type='text'
+          placeholder='Prompt'
+          name='selectedLocation'
+          value={typed}
+          onChange={event => {
+            location.setSelectedLocation(event.target.value)
+            setTyped(event.target.value)
+            console.log(event.target.value)
+          }}
+        />
 
-      <button
-        onClick={submitLocation}
-        disabled={!location.selectedLocation}
-        className='submit-button'
-      >
-        SUBMIT
-      </button>
-    </form>
+        <button
+          onClick={submitLocation}
+          disabled={!location.selectedLocation}
+          className='submit-button'
+        >
+          SUBMIT
+        </button>
+        <button
+          onClick={event => {
+            event.preventDefault()
+            location.setSelectedLocation('')
+          }}
+        >
+          Use Current Location
+        </button>
+      </form>
+    </div>
   )
 }
- export default Form
+export default Form
