f43d77a9714e91d950884b59b60d81e75080cfa5
Casey Halstead <catherineclosee@gmail.com>
2022-05-19 08:43:15 -0500

    Add tests for homepage spec

--- cypress/integration/main_spec.js
+++ cypress/integration/main_spec.js
@@ -1,6 +1,14 @@
 describe('DiverCity homepage flow', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://immense-falls-83363.herokuapp.com/api/v1/businesses?location=denver}&category=shopping', {fixture: 'sampleBusinessesData.json' })
     cy.visit('http://localhost:3000')
+  })
+
+  it('should be able to visit the page and view the Title, business categories and buttons', () => {
+    cy.contains('DiverCity: Inclusive Business Guide')
+    cy.contains('Pick your category')
+    cy.get('h1').contains('Food & Drink')
+    cy.get('h1').contains('Shopping')
+    cy.get('h1').contains('Arts & Entertainment')
+    cy.get('button').should('have.length', 3).and('contain', 'View All')
   })
 })
