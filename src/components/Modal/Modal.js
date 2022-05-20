c52aa8c79155fc2f4ca3ba4b771482f1efd3f114
Kim Ward <kmewrd@outlook.com>
2022-05-20 18:59:32 -0500

    Use id from params to complete fetch

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from 'react';
 import { fetchBusiness } from '../../apiCalls';
+import { useParams } from 'react-router-dom';
 
-const Modal = ({ id }) => {
+const Modal = () => {
+  const { id } = useParams();
+
   const [business, setBusiness] = useState(null);
   const [error, setError] = useState(null);
 
@@ -10,6 +13,8 @@ const Modal = ({ id }) => {
       .then(data => setBusiness(data))
       .catch(err => setError('Oops, something went wrong! Please try again later.'))
   }, [])
+
+  console.log(business)
 
   return (
     <div>
