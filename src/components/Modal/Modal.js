c3be12af9928f8d9359164ec4cf3918877cc5452
Kim Ward <kmewrd@outlook.com>
2022-05-20 19:14:40 -0500

    Render business data

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchBusiness } from '../../apiCalls';
 import { useParams } from 'react-router-dom';
 
@@ -10,7 +10,7 @@ const Modal = () => {
 
   useEffect(() => {
     fetchBusiness(id)
-      .then(data => setBusiness(data))
+      .then(data => setBusiness(data.data.attributes))
       .catch(err => setError('Oops, something went wrong! Please try again later.'))
   }, [])
 
@@ -19,6 +19,12 @@ const Modal = () => {
   return (
     <div>
       {error && <p>{error}</p>}
+      {business && (<>
+        <h2>{business.name}</h2>
+        <img src={business.image} />
+        <h3>Location: {business.location}</h3>
+        <h3>Contact: {business.phone}</h3>
+        </>)}
     </div>
   )
 }
