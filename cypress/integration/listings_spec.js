413b731d5872052dad5866780f0f3c7c6bb7a582
Ronald Head <ronaldlhead@gmail.com>
2022-05-22 15:00:36 -0600

    Add test to display sub menu after Show All select  element is clicked. Sub menu displays 5 options: American (New), Breakfast & Brunch, Caribbean, Coffee & Tea, and Sandwiches

--- cypress/integration/listings_spec.js
+++ cypress/integration/listings_spec.js
@@ -7,38 +7,54 @@ describe('listings view', () => {
       .get('.submit-button').click()
   })
 
-  it('should display search results for the selected city with business name, image, and button', () => {
-    cy.get('.category-card:first button').click()
-      .get('.listings-container')
-      .should('contain', 'Food & Drink')
-      .get('.business-card:first')
-      .should('contain', 'Jelly')
-      .and('have.descendants', 'img')
-      .and('have.descendants', 'button')
-  })
+  // it('should display search results for the selected city with business name, image, and button', () => {
+  //   cy.get('.category-card:first button').click()
+  //     .get('.listings-container')
+  //     .should('contain', 'Food & Drink')
+  //     .get('.business-card:first')
+  //     .should('contain', 'Jelly')
+  //     .and('have.descendants', 'img')
+  //     .and('have.descendants', 'button')
+  // })
 
-  it('should display error handling if the city entered is misspelled', () => {
-    cy.get('form input')
-      .type('Dener')
-      .get('.submit-button').click()
+  // it('should display error handling if the city entered is misspelled', () => {
+  //   cy.get('form input')
+  //     .type('Dener')
+  //     .get('.submit-button').click()
+  //   cy.get('.category-card:first button').click()
+  //     .url('http://localhost:3000/food')
+  //     .get('h3')
+  //     .should('contain', 'No results for your search. Please check your spelling and try a new search.')
+  // })
+
+  // it('should display error handling if complete gibberish is entered for the city search.', () => {
+  //   cy.get('form input')
+  //     .type('asdfasdfasdf')
+  //     .get('.submit-button').click()
+  //   cy.get('.category-card:first button').click()
+  //     .url('http://localhost:3000/food')
+  //     .get('h3')
+  //     .should('contain', 'Oops, something went wrong! Please try again later.')
+  // })
+
+  // it('should change the url when viewing a listings page', () => {
+  //   cy.get('.category-card:first button').click()
+  //     .url('http://localhost:3000/food')
+  // })
+
+  it('should display sub menu if Show All is clicked', () => {
     cy.get('.category-card:first button').click()
       .url('http://localhost:3000/food')
-      .get('h3')
-      .should('contain', 'No results for your search. Please check your spelling and try a new search.')
-  })
-
-  it('should display error handling if complete gibberish is entered for the city search.', () => {
-    cy.get('form input')
-      .type('asdfasdfasdf')
-      .get('.submit-button').click()
-    cy.get('.category-card:first button').click()
-      .url('http://localhost:3000/food')
-      .get('h3')
-      .should('contain', 'Oops, something went wrong! Please try again later.')
-  })
-
-  it('should change the url when viewing a listings page', () => {
-    cy.get('.category-card:first button').click()
-      .url('http://localhost:3000/food')
+    cy.get('select').select('Show All')
+      .get('option')
+        .should('contain', 'American (New)')
+      .get('option')
+        .should('contain', 'Breakfast & Brunch')
+      .get('option')
+        .should('contain', 'Caribbean')
+      .get('option')
+        .should('contain', 'Coffee & Tea')
+      .get('option')
+        .should('contain', 'Sandwiches')
   })
 })
