9be87dc252f1ddbbbd02d50974dba1ddce5b2cbd
Ronald Head <ronaldlhead@gmail.com>
2022-05-22 15:09:06 -0600

    Add test to display businesses ffor sub categories. When the user selects American (New) it will display one restaurant in stubbed data (Jelly). When the user selects Caribbean it will display one restaurant in stubbed data (Jamaican Grill)

--- cypress/integration/listings_spec.js
+++ cypress/integration/listings_spec.js
@@ -7,40 +7,40 @@ describe('listings view', () => {
       .get('.submit-button').click()
   })
 
-  // it('should display search results for the selected city with business name, image, and button', () => {
-  //   cy.get('.category-card:first button').click()
-  //     .get('.listings-container')
-  //     .should('contain', 'Food & Drink')
-  //     .get('.business-card:first')
-  //     .should('contain', 'Jelly')
-  //     .and('have.descendants', 'img')
-  //     .and('have.descendants', 'button')
-  // })
+  it('should display search results for the selected city with business name, image, and button', () => {
+    cy.get('.category-card:first button').click()
+      .get('.listings-container')
+      .should('contain', 'Food & Drink')
+      .get('.business-card:first')
+      .should('contain', 'Jelly')
+      .and('have.descendants', 'img')
+      .and('have.descendants', 'button')
+  })
 
-  // it('should display error handling if the city entered is misspelled', () => {
-  //   cy.get('form input')
-  //     .type('Dener')
-  //     .get('.submit-button').click()
-  //   cy.get('.category-card:first button').click()
-  //     .url('http://localhost:3000/food')
-  //     .get('h3')
-  //     .should('contain', 'No results for your search. Please check your spelling and try a new search.')
-  // })
+  it('should display error handling if the city entered is misspelled', () => {
+    cy.get('form input')
+      .type('Dener')
+      .get('.submit-button').click()
+    cy.get('.category-card:first button').click()
+      .url('http://localhost:3000/food')
+      .get('h3')
+      .should('contain', 'No results for your search. Please check your spelling and try a new search.')
+  })
 
-  // it('should display error handling if complete gibberish is entered for the city search.', () => {
-  //   cy.get('form input')
-  //     .type('asdfasdfasdf')
-  //     .get('.submit-button').click()
-  //   cy.get('.category-card:first button').click()
-  //     .url('http://localhost:3000/food')
-  //     .get('h3')
-  //     .should('contain', 'Oops, something went wrong! Please try again later.')
-  // })
+  it('should display error handling if complete gibberish is entered for the city search.', () => {
+    cy.get('form input')
+      .type('asdfasdfasdf')
+      .get('.submit-button').click()
+    cy.get('.category-card:first button').click()
+      .url('http://localhost:3000/food')
+      .get('h3')
+      .should('contain', 'Oops, something went wrong! Please try again later.')
+  })
 
-  // it('should change the url when viewing a listings page', () => {
-  //   cy.get('.category-card:first button').click()
-  //     .url('http://localhost:3000/food')
-  // })
+  it('should change the url when viewing a listings page', () => {
+    cy.get('.category-card:first button').click()
+      .url('http://localhost:3000/food')
+  })
 
   it('should display sub menu if Show All is clicked', () => {
     cy.get('.category-card:first button').click()
@@ -56,5 +56,16 @@ describe('listings view', () => {
         .should('contain', 'Coffee & Tea')
       .get('option')
         .should('contain', 'Sandwiches')
+  })
+
+  it('should only display businesses for a specific sub category after it is clicked', () => {
+    cy.get('.category-card:first button').click()
+    .url('http://localhost:3000/food')
+    cy.get('select').select('American (New)')
+      .get('.business-card:first')
+      .should('contain', 'Jelly')
+    cy.get('select').select('Caribbean')
+      .get('.business-card:first')
+      .should('contain', 'Jamaican Grill')
   })
 })
