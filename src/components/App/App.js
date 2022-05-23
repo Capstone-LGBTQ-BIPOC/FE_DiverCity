012cb21b0b9cb081eac6074b9ae710dd5c94c439
Alison Nooner <alinooner@gmail.com>
2022-05-23 15:25:47 -0500

    Refactor

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,39 +1,37 @@
-import Categories from '../Categories/Categories'
-import Listings from '../Listings/Listings'
-import Modal from '../Modal/Modal'
-import Bookmarks from '../Bookmarks/Bookmarks'
-import './App.css'
-import { LocationContextProvider } from '../../contexts/LocationContext'
-import { BusinessContextProvider } from '../../contexts/BusinessContext'
-import { BookmarkContextProvider } from '../../contexts/BookmarkContext'
-import { Routes, Route, NavLink } from 'react-router-dom'
-import Form from '../Form/Form'
+import Categories from "../Categories/Categories";
+import Listings from "../Listings/Listings";
+import Modal from "../Modal/Modal";
+import Bookmarks from "../Bookmarks/Bookmarks";
+import "./App.css";
+import { LocationContextProvider } from "../../contexts/LocationContext";
+import { BusinessContextProvider } from "../../contexts/BusinessContext";
+import { BookmarkContextProvider } from "../../contexts/BookmarkContext";
+import { Routes, Route, NavLink } from "react-router-dom";
+import Form from "../Form/Form";
 
 const App = () => {
   return (
     <LocationContextProvider>
       <BusinessContextProvider>
-
-      <BookmarkContextProvider>
-        <main>
-          <header>
-            <NavLink to='/'>
-              <h1>DiverCity: Inclusive Business Guide</h1>
-            </NavLink>
-          </header>
-          <Form />
-          <Routes>
-            <Route exact path='/' element={<Categories />} />
-            <Route path='/:category' element={<Listings />} />
-            <Route path='/biz/:id' element={<Modal />} />
-            <Route exact path='/bookmarks' element={<Bookmarks />}/>
-          </Routes>
-        </main>
-      </BookmarkContextProvider>
-
+        <BookmarkContextProvider>
+          <main>
+            <header>
+              <NavLink to="/">
+                <h1>DiverCity: Inclusive Business Guide</h1>
+              </NavLink>
+            </header>
+            <Form />
+            <Routes>
+              <Route exact path="/" element={<Categories />} />
+              <Route path="/:category" element={<Listings />} />
+              <Route path="/biz/:id" element={<Modal />} />
+              <Route exact path="/bookmarks" element={<Bookmarks />} />
+            </Routes>
+          </main>
+        </BookmarkContextProvider>
       </BusinessContextProvider>
     </LocationContextProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
