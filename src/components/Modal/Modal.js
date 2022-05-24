b4978d8439ce9f2751f03b269327a68e4b4ec0bf
Ronald Head <ronaldlhead@gmail.com>
2022-05-24 10:02:31 -0600

    Refactor bookmark functionality to single business view.

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { BookmarkContext } from '../../contexts/BookmarkContext';
 import { fetchBusiness } from '../../apiCalls';
 import { useParams, useNavigate } from 'react-router-dom';
 
 const Modal = () => {
   const { id } = useParams();
   let navigate = useNavigate();
+  const bookmark = useContext(BookmarkContext)
 
   const [business, setBusiness] = useState(null);
   const [error, setError] = useState(null);
@@ -26,7 +28,9 @@ const Modal = () => {
         <h3>Location: {business.location}</h3>
         <h3>Contact: {business.phone}</h3>
         <a href={business.url} target='_blank'>Visit Website</a>
-        </>)}
+        <button className='single-biz-bookmark-button'
+        onClick={() => {bookmark.updateBookmark(id)}}>Bookmark</button>
+      </>)}
     </div>
   )
 }
