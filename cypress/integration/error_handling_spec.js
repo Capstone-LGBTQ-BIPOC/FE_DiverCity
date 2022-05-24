1fb30eaa44b43b19a175e996c51b4998b8281066
Ronald Head <ronaldlhead@gmail.com>
2022-05-23 19:59:20 -0600

    Add error handling tests: moved error handling tests from listings_spec.js to error_handling_spec.js since this file will house all error handling tests

---
+++ cypress/integration/error_handling_spec.js
@@ -0,0 +1,31 @@
+describe('Error handling', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://www.geoplugin.net/json.gp', { fixture: 'sampleGeoData.json' }).as('location data')
+    cy.intercept('GET', 'https://immense-falls-83363.herokuapp.com/api/v1/businesses?location=Denver&category=food', { fixture: 'sampleFoodData.json' }).as('food results')
+    cy.intercept('GET', 'https://immense-falls-83363.herokuapp.com/api/v1/businesses?location=asdfasdfasdf&category=food', { forceNetworkError: true }).as('nonexistent city')
+    cy.intercept('GET', 'https://immense-falls-83363.herokuapp.com/api/v1/businesses?location=Dener&category=food', { fixture: 'noResults.json' }).as('no results')
+    cy.visit('http://localhost:3000')
+      .get('form input').type('Denver')
+      .get('.submit-button').click()
+  })
+  
+  it('should display an error message if the city entered is misspelled', () => {
+    cy.get('form input')
+      .type('Dener')
+      .get('.submit-button').click()
+    cy.get('.category-button:first').click()
+      .url('http://localhost:3000/food')
+      .get('h3')
+      .should('contain', 'No results for your search. Please check your spelling and try a new search.')
+  })
+  
+  it('should display an error message if complete gibberish is entered for the city search.', () => {
+      cy.get('form input')
+        .type('asdfasdfasdf')
+        .get('.submit-button').click()
+      cy.get('.category-button:first').click()
+        .url('http://localhost:3000/food')
+        .get('h3')
+        .should('contain', 'Oops, something went wrong! Please try again later.')
+    })
+})
