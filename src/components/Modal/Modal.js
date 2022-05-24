43333ff0848a217ef1fd9757a5146fed7fc2bcd6
Casey Halstead <catherineclosee@gmail.com>
2022-05-24 10:34:06 -0500

    Add logic for converting time string to standard time hours

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -5,8 +5,11 @@ import { useParams, useNavigate } from 'react-router-dom';
 const Modal = () => {
   const { id } = useParams();
   let navigate = useNavigate();
+  let hoursDisplay;
+  let openTime;
+  let closeTime;
 
-  const [business, setBusiness] = useState(null);
+  const [business, setBusiness] = useState('');
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -15,7 +18,45 @@ const Modal = () => {
       .catch(err => setError('Oops, something went wrong! Please try again later.'))
   }, [])
 
-  console.log(business)
+  if(business) {
+    const bizHours = Object.keys(business.hours)
+      hoursDisplay = bizHours.map(day => {
+      if(business.hours[day].open > 1300) {
+        const openPM = parseInt(business.hours[day].open) - 1200
+        if(openPM.toString().length === 3) {
+          openTime = openPM.toString().substring(0, 1) + ":" + openPM.toString().substring(1, 3) + "pm"
+        } else (
+         openTime = openPM.toString().substring(0, 2) + ":" + openPM.toString().substring(2, 4) + "pm"
+        )
+      } else if (business.hours[day].open < 1200) {
+        openTime = business.hours[day].open.substring(0, 2) + ":" + business.hours[day].open.substring(2,4) + "am"
+      } else if (business.hours[day].open === "0000") {
+        return "N/A"
+      } else {
+        closeTime = business.hours[day].close.substring(0, 2) + ":" + business.hours[day].close.substring(2, 4) + "pm"
+      }
+
+      if(business.hours[day].close > 1300) {
+       const closePM = parseInt(business.hours[day].close) - 1200
+       if(closePM.toString().length === 3) {
+         closeTime = closePM.toString().substring(0, 1) + ":" + closePM.toString().substring(1, 3) + "pm"
+        } else (
+        closeTime = closePM.toString().substring(0, 2) + ":" + closePM.toString().substring(2, 4) + "pm"
+        )
+      } else if (business.hours[day].close < 1200) {
+       closeTime = business.hours[day].close.substring(1, 2) + ":" + business.hours[day].close.substring(2,4) + "am"
+        } else if (business.hours[day].close === "0000") {
+          return "N/A"
+        } else {
+          closeTime = business.hours[day].close.substring(0, 2) + ":" + business.hours[day].close.substring(2, 4) + "pm"
+        }
+        return (
+          <div key={day}>
+            <p>{day}: {openTime} - {closeTime}</p>
+          </div>
+        )
+    })
+  }
 
   return (
     <div>
@@ -26,6 +67,8 @@ const Modal = () => {
         <img src={business.image} />
         <h3>{business.category}</h3>
         <h3>Location: {business.location}</h3>
+        <h3>Hours:</h3>
+        {hoursDisplay}
         <h3>Contact: {business.phone}</h3>
         <a href={business.url} target='_blank'>Visit Website</a>
         </>)}
